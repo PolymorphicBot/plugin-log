91471a737fcd0e6e4899b194d73f60157098321c
Kenneth Endfinger <kaendfinger@gmail.com>
2015-01-20 18:58:45 -0500

    Initial Commit

---
+++ main.dart
@@ -0,0 +1,64 @@
+import "dart:async";
+import "dart:collection";
+import "dart:io";
+
+import "package:polymorphic_bot/api.dart";
+
+@PluginInstance()
+Plugin plugin;
+
+main(args, port) => polymorphic(args, port);
+
+Timer _timer;
+
+@Start()
+void start() {
+  plugin.log("Loading");
+}
+
+@Start()
+void startTimer() {
+  _timer = new Timer.periodic(new Duration(seconds: 5), (_) {
+    var map = <String, List<MessageEvent>>{};
+    
+    while (_msgQueue.isNotEmpty) {
+      var event = _msgQueue.removeFirst();
+      var target = event.isPrivate ? "@${event.target}" : event.target.substring(1);
+      var simpleName = "${event.network}/${target}";
+      
+      if (map.containsKey(simpleName)) {
+        map[simpleName].add(event);
+      } else {
+        map[simpleName] = <MessageEvent>[event];
+      }
+    }
+    
+    for (var name in map.keys) {
+      var file = new File("logs/${name}");
+      
+      if (!file.existsSync()) {
+        file.createSync(recursive: true);
+      }
+      
+      var content = map[name].map((event) {
+        return "<${event.from}> ${DisplayHelpers.clean(event.message)}";
+      }).join("\n") + "\n";
+      
+      file.writeAsStringSync(content, mode: FileMode.APPEND);
+    }
+  });
+}
+
+@EventHandler("shutdown")
+void stopTimer() {
+  if (_timer.isActive) {
+    _timer.cancel();
+  }
+}
+
+@OnMessage()
+void handleMessage(MessageEvent event) {
+  _msgQueue.add(event);
+}
+
+Queue<MessageEvent> _msgQueue = new Queue<MessageEvent>();
